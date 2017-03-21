6a46aef2ea13ace1d171a6db62eebd8d5dad0d0c
TT <dyw564@126.com>
2017-03-21 23:17:14 +0800

    first commit

---
+++ khufu/dao/models/base.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+"""
+Create at 2017/3/21
+这里做一个base model 其他model都集成base model
+"""
+
+__author__ = 'TT'
+
+from django.db.models import Model, DateTimeField, BooleanField
+
+
+class Base(Model):
+    """"""
+    created_at = DateTimeField(null=True, blank=True, auto_now_add=True)
+    deleted_at = DateTimeField(null=True, blank=True)
+
+    is_enable = BooleanField(default=True)
+    is_delete = BooleanField(default=False)
